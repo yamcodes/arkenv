d794221191ff3c2795fff225233809631ac72179
Yam Borodetsky <yam@yam.codes>
2025-01-25 20:55:28 +0500

    Update Changesets configuration and add changelog package

--- src/sample/index.ts
+++ src/sample/index.ts
@@ -2,14 +2,14 @@ import { type } from "arktype";
 import { defineEnv } from "..";
 
 const User = type({
-  name: "string",
-  age: "number",
-  friends: "string[]",
+	name: "string",
+	age: "number",
+	friends: "string[]",
 });
 export type User = typeof User.infer;
 
 const myEnv = defineEnv({
-  HELLO: "string",
+	HELLO: "string",
 });
 
 console.log(myEnv);
