f0552ea0b8c37603e3c97dcda7585dd11b2a9e02
Yam Borodetsky <yam@yam.codes>
2025-02-09 02:32:42 +0500

    Revert "ci(docs): fix base path"
    This reverts commit c5613e3b1453eabd195553e6e79b1a877821568f.

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,27 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-    output: 'export',  
-    /**
-    * Set base path. This is the slug of your GitHub repository.
-    *
-    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: '/ark.env',
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-   images: {
-    unoptimized: true,
-  },
+  output: "export",
 };
 
 export default withMDX(config);
