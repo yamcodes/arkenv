bbcb39f95ab45a2424799d851bb5ef48828494a5
Yam Borodetsky <yam@yam.codes>
2025-02-08 20:17:20 +0500

    refactor: update import from local to package

--- src/sample.ts
+++ src/sample.ts
@@ -1,5 +1,5 @@
 import { blue, bold, green, red } from "picocolors";
-import { defineEnv, host, port } from "./index";
+import { defineEnv, host, port } from "ark.env";
 
 const env = defineEnv({
 	HOST: host,
