5ceb796a92e8c57fc5d071249e1561ec0f57f5e3
Yam Borodetsky <yam@yam.codes>
2025-02-10 00:17:02 +0500

    docs: Add root redirect to BASE_URL in next config
    Closes #18

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -4,4 +4,17 @@ import { BASE_URL } from "~/config/constants";
 export default createMDX()({
 	reactStrictMode: true,
 	basePath: BASE_URL,
+	async redirects() {
+		return [
+			// This is pretty much just for the local dev experience
+			// Will redirect localhost:3000 to localhost:3000/ark.env
+			// To support our basePath
+			{
+				source: "/",
+				destination: BASE_URL,
+				permanent: true,
+				basePath: false,
+			},
+		];
+	},
 });
