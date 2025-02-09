a4e8a433bacffe555dc60cf5b5f5717e91fd3d7d
Yam Borodetsky <yam@yam.codes>
2025-02-09 23:22:25 +0500

    fix: rename global.css to globals.css in docs app

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,4 +1,4 @@
-import "./global.css";
+import "./globals.css";
 import nextConfig from "@/next.config";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
