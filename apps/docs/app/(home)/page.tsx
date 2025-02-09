97ccc0670a76a06c1bfd7bd5be664a43de113686
Yam Borodetsky <yam@yam.codes>
2025-02-09 23:54:18 +0500

    Add shadcn

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 export const metadata: Metadata = {
 	title: "ark.env: Typesafe Environment Variables",
@@ -14,13 +15,10 @@ export default function HomePage() {
 				<code className="text-fd-foreground">ark.env</code>
 			</h1>
 			<p className="text-fd-muted-foreground">Typesafe Environment Variables</p>
-			<div className="flex justify-center">
-				<Link
-					href="/docs"
-					className="inline-block bg-fd-foreground text-fd-background px-4 py-2 rounded-md mt-4"
-				>
-					Set sail --&gt;
-				</Link>
+			<div className="flex justify-center mt-4">
+				<Button asChild>
+					<Link href="/docs">Set sail --&gt;</Link>
+				</Button>
 			</div>
 		</main>
 	);
