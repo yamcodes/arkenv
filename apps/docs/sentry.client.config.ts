71103ea5c89efaf858961c61744877da19d55dda
Yam Borodetsky <yam@yam.codes>
2025-02-15 23:37:18 +0500

    chore: Disable Sentry debug mode across client, edge, and server configs

--- apps/docs/sentry.client.config.ts
+++ apps/docs/sentry.client.config.ts
@@ -34,5 +34,5 @@ Sentry.init({
 			: 1,
 
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
-	debug: process.env.NODE_ENV === "development",
+	debug: false,
 });
