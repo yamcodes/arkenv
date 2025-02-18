96c3668f206bb4bd7d5fc7e811b362fcc64f3532
Yam Borodetsky <yam@yam.codes>
2025-02-19 03:25:28 +0500

    feat(sail-button): Adjust sailboat animation duration

--- apps/docs/components/page/sail-button.tsx
+++ apps/docs/components/page/sail-button.tsx
@@ -38,7 +38,7 @@ export function SailButton() {
 				className="text-lg font-bold relative overflow-hidden cursor-pointer dark:bg-primary dark:text-primary-foreground dark:hover:bg-primary/80 dark:hover:text-primary-foreground transition-all duration-300 shadow-[0_4px_20px_rgba(96,165,250,0.6)] dark:shadow-[0_16px_20px_rgba(96,165,250,0.6)] hover:bg-blue-500/10 bg-gradient-to-r from-white/20 to-transparent"
 			>
 				<Sailboat
-					className={`transition-transform duration-[1500ms] ease-in-out ${isSailing ? "translate-x-[1000%]" : ""}`}
+					className={`transition-transform duration-[700ms] ease-in ${isSailing ? "translate-x-[1000%]" : ""}`}
 					onTransitionEnd={() => router.push("/docs/quickstart")}
 				/>
 				<span>
