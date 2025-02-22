666098d5b386755056ca60e7160a0ad734439eb0
Yam Borodetsky <yam@yam.codes>
2025-02-22 19:27:58 +0500

    chore: Update project configuration and example project

--- examples/with-vite-react-ts/vite.config.ts
+++ examples/with-vite-react-ts/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { ValidateEnv as validateEnv } from "@julr/vite-plugin-validate-env";
+import react from "@vitejs/plugin-react";
 import { type } from "arktype";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
