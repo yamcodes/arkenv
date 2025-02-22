dc5327aacc993a6c29a94a769d2b3abd81d7737a
Yam Borodetsky <yam@yam.codes>
2025-02-23 01:37:59 +0500

    feat(vite-plugin): Improve environment loading with Vite's loadEnv

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -1,6 +1,6 @@
 import ark from "ark.env";
 import type { type } from "arktype";
-import type { Plugin } from "vite";
+import { type Plugin, loadEnv } from "vite";
 
 // This is the same `EnvSchema` type as in `ark.env`.
 // We are re-defining it here since TypeScript loses inference when using the type from `ark.env`.
@@ -14,7 +14,7 @@ type EnvSchema<
 
 export default <T extends EnvSchema>(options: EnvSchema<T>): Plugin => ({
 	name: "@arkenv/vite-plugin",
-	config() {
-		ark.env(options);
+	config(_config, { mode }) {
+		ark.env(options, loadEnv(mode, process.cwd(), ""));
 	},
 });
