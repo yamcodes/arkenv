f4c672197b5510d6faa81d866cc7518b7da5bbe0
Yam Borodetsky <yam@yam.codes>
2025-09-08 23:20:22 +0500

    refactor: remove types export from index.ts
    This commit updates the `index.ts` file by removing the export of types, streamlining the default export to only include environment-related functionalities. This change simplifies the module's interface and focuses on core features.

--- packages/arkenv/src/index.ts
+++ packages/arkenv/src/index.ts
@@ -1,10 +1,8 @@
 import * as env from "./create-env";
-import * as types from "./types";
 
 // Export all exports as a default export
-export default { ...env, ...types };
+export default { ...env };
 
 export type { EnvSchema } from "./create-env";
 // Also export as named exports
 export * from "./create-env";
-export * from "./types";
