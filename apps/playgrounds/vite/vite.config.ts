50659dfc7c00d88fca8c8a0c297d0324c304522e
Yam Borodetsky <yam@yam.codes>
2025-09-12 11:37:04 +0500

    Add Vite configuration and environment setup
    - Introduced a new .env.development file to set the PORT for the Vite application.
    - Added a "start" script to package.json for easier application startup.
    - Integrated the @arkenv/vite-plugin into vite.config.ts for enhanced environment variable management.

--- apps/playgrounds/vite/vite.config.ts
+++ apps/playgrounds/vite/vite.config.ts
@@ -1,7 +1,13 @@
+import arkenv from "@arkenv/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		arkenv({
+			PORT: "number.port",
+		}),
+	],
 });
