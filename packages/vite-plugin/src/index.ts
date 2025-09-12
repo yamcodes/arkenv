3877886a4fdc0201de900e7c984004d068e589fe
Yam Borodetsky <yam@yam.codes>
2025-09-12 12:04:23 +0500

    Refactor Vite plugin export to use named function syntax
    - Changed the default export of the Vite plugin to a named function `arkenv`, improving readability and maintainability.
    - The function signature remains consistent with the previous implementation, ensuring compatibility with existing usage.

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -2,11 +2,13 @@ import type { EnvSchema } from "arkenv";
 import { createEnv } from "arkenv";
 import { loadEnv, type Plugin } from "vite";
 
-export default <const T extends Record<string, string | undefined>>(
-	options: EnvSchema<T>,
-): Plugin => ({
-	name: "@arkenv/vite-plugin",
-	config(_config, { mode }) {
-		createEnv(options, loadEnv(mode, process.cwd(), ""));
-	},
-});
+export default function arkenv<
+	const T extends Record<string, string | undefined>,
+>(options: EnvSchema<T>): Plugin {
+	return {
+		name: "@arkenv/vite-plugin",
+		config(_config, { mode }) {
+			createEnv(options, loadEnv(mode, process.cwd(), ""));
+		},
+	};
+}
