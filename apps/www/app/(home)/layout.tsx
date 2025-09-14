fd7443779a710870fada738d1a3afb28b358c6ca
Yam Borodetsky <yam@yam.codes>
2025-09-15 03:00:27 +0500

    Remove "Documentation" button in the sidebar (#167)
    - Reintroduced the "Documentation" link in the HomeLayout component by
    integrating it directly into the layout configuration.
    - This change enhances navigation by providing users with easy access to
    the documentation from the home layout.
    
    Closes #166

--- apps/www/app/(home)/layout.tsx
+++ apps/www/app/(home)/layout.tsx
@@ -3,5 +3,18 @@ import type { ReactNode } from "react";
 import { baseOptions } from "~/app/layout.config";
 
 export default function Layout({ children }: { children: ReactNode }) {
-	return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+	return (
+		<HomeLayout
+			{...baseOptions}
+			links={[
+				{
+					text: "Documentation",
+					url: "/docs",
+					active: "none",
+				},
+			]}
+		>
+			{children}
+		</HomeLayout>
+	);
 }
