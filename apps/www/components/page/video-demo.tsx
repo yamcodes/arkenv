4d264ea8bf5d17991c4f54e1f7ba9579d312c91a
Yam C Borodetsky <yam@yam.codes>
2025-09-24 00:10:08 +0300

    fix: update video source in VideoDemo component
    - Changed video source to a new URL for improved accessibility and reliability.

--- apps/www/components/page/video-demo.tsx
+++ apps/www/components/page/video-demo.tsx
@@ -25,7 +25,10 @@ export function VideoDemo() {
 					poster="/assets/demo.png"
 					className="block max-h-[600px] sm:max-h-[1000px] object-contain"
 				>
-					<source src="/assets/demo.mp4" type="video/mp4" />
+					<source
+						src="https://x9fkbqb4whr3w456.public.blob.vercel-storage.com/hero.mp4"
+						type="video/mp4"
+					/>
 					You need a browser that supports HTML5 video to view this video.
 				</video>
 			</button>
