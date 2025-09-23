d53b4e868958c6aa0028229a64a65b4f80237c6a
Yam C Borodetsky <yam@yam.codes>
2025-09-23 19:25:54 +0300

    feat: make video demo clickable
    - Wrapped video in a button
    - Allows play on click

--- apps/www/components/page/video-demo.tsx
+++ apps/www/components/page/video-demo.tsx
@@ -13,7 +13,8 @@ export function VideoDemo() {
 			<div className="absolute inset-0 rounded-lg pointer-events-none shadow-[0_0_20px_rgba(96,165,250,0.6)] dark:shadow-[0_0_100px_rgba(96,165,250,0.2)]" />
 
 			{/* Main video container */}
-			<div
+			<button
+				type="button"
 				className="rounded-lg overflow-hidden border border-fd-border shadow-lg bg-black/5 dark:bg-black/20 cursor-pointer"
 				onClick={handleVideoClick}
 			>
@@ -28,7 +29,7 @@ export function VideoDemo() {
 					<source src="/arkenv-video.mov" type="video/mp4" />
 					Your browser does not support the video tag.
 				</video>
-			</div>
+			</button>
 		</div>
 	);
 }
