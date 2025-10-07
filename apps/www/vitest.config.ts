4ad02b146e089a3bde48e26cff135baae12bb75d
Yam Borodetsky <yam@yam.codes>
2025-10-07 20:38:26 +0500

    chore: update dependencies and configuration for improved functionality
    - Added new dependencies: @swc/plugin-styled-jsx, @testing-library/dom, styled-jsx, and vite-tsconfig-paths in package.json files.
    - Updated pnpm-lock.yaml to include new versions and resolutions for added dependencies.
    - Modified Vitest configuration to integrate vite-tsconfig-paths for better path resolution.

--- apps/www/vitest.config.ts
+++ apps/www/vitest.config.ts
@@ -1,17 +1,17 @@
-import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineProject } from "vitest/config";
 
 export default defineProject({
-	plugins: [react()],
+	plugins: [
+		react({
+			plugins: [["@swc/plugin-styled-jsx", {}]],
+		}),
+		tsconfigPaths(),
+	],
 	test: {
 		name: "arkenv.js.org",
 		environment: "jsdom",
 		setupFiles: ["./tests/setup.ts"],
-	},
-	resolve: {
-		alias: {
-			"~": path.resolve(__dirname, "."),
-		},
 	},
 });
