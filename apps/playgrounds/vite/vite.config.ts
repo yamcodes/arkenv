647c16b3cf6fe9ef8e8657edf743132923be2a83
Yam C Borodetsky <yam@yam.codes>
2025-10-13 19:33:21 +0500

    Update pnpm-lock.yaml and add new environment variables to Vite playground
    - Updated dependencies in pnpm-lock.yaml, including vitest and jsdom.
    - Added new environment variables (VITE_MY_NUMBER, VITE_MY_VAR, VITE_MY_BOOLEAN) to the Vite development environment.
    - Enhanced the Vite configuration to utilize the new environment variables.
    - Updated the App component to display the new environment variables.
    
    These changes improve the development experience and ensure compatibility with the latest dependencies.

--- apps/playgrounds/vite/vite.config.ts
+++ apps/playgrounds/vite/vite.config.ts
@@ -1,5 +1,6 @@
 import arkenv from "@arkenv/vite-plugin";
 import react from "@vitejs/plugin-react";
+import { type } from "arkenv";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -7,7 +8,9 @@ export default defineConfig({
 	plugins: [
 		react(),
 		arkenv({
-			PORT: "number.port",
+			VITE_MY_VAR: "string",
+			VITE_MY_NUMBER: type("string").pipe((str) => Number.parseInt(str, 10)),
+			VITE_MY_BOOLEAN: type("string").pipe((str) => str === "true"),
 		}),
 	],
 });
