bca5d5640f2deecd619dd8dd6989c8542c49f38d
Yam C Borodetsky <yam@yam.codes>
2025-11-13 00:20:29 +0500

    refactor(vite.config.ts): enhance configuration with environment loading and improved plugin usage
    - Updated Vite configuration to utilize `loadEnv` for environment variable management.
    - Refactored plugin imports and initialization for better clarity and structure.
    - Ensured server port is dynamically set based on environment variables.

--- apps/playgrounds/vite/vite.config.ts
+++ apps/playgrounds/vite/vite.config.ts
@@ -1,16 +1,31 @@
-import arkenv from "@arkenv/vite-plugin";
-import react from "@vitejs/plugin-react";
-import { type } from "arkenv";
-import { defineConfig } from "vite";
+import ArkEnv from "@arkenv/vite-plugin";
+import React from "@vitejs/plugin-react";
+import arkenv, { type } from "arkenv";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [
-		react(),
-		arkenv({
+export default defineConfig(({ mode }) => {
+	const env = arkenv(
+		{
+			PORT: "number.port",
 			VITE_MY_VAR: "string",
 			VITE_MY_NUMBER: type("string").pipe((str) => Number.parseInt(str, 10)),
 			VITE_MY_BOOLEAN: type("string").pipe((str) => str === "true"),
-		}),
-	],
+		},
+		loadEnv(mode, process.cwd(), ""),
+	);
+	return {
+		plugins: [
+			React(),
+			ArkEnv({
+				PORT: "number.port",
+				VITE_MY_VAR: "string",
+				VITE_MY_NUMBER: type("string").pipe((str) => Number.parseInt(str, 10)),
+				VITE_MY_BOOLEAN: type("string").pipe((str) => str === "true"),
+			}),
+		],
+		server: {
+			port: env.PORT,
+		},
+	};
 });
