e2722e10e450a1657118c83aca9707ecd5aed185
Yam C Borodetsky <yam@yam.codes>
2025-11-22 12:17:49 +0500

    feat: add icons to documentation metadata
    - Included icons for various documentation pages in ArkEnv and Vite plugin, enhancing visual representation.
    - Updated the source file to support dynamic icon imports from the SimpleIcons package.

--- apps/www/lib/source.tsx
+++ apps/www/lib/source.tsx
@@ -1,4 +1,5 @@
 import type { autocomplete } from "@ark/util";
+import * as SimpleIcons from "@icons-pack/react-simple-icons";
 import { loader } from "fumadocs-core/source";
 import { icons } from "lucide-react";
 import { createElement } from "react";
@@ -14,6 +15,9 @@ export const source = loader({
 		if (!icon) return;
 
 		if (icon in icons) return createElement(icons[icon as never]);
+		if (`Si${icon}` in SimpleIcons)
+			// biome-ignore lint/performance/noDynamicNamespaceImportAccess: I don't care about bundle size
+			return createElement(SimpleIcons[`Si${icon}` as never]);
 		if (icon === "New")
 			return <Badge className="h-4 text-[10px] px-[0.2rem] order-1">new</Badge>;
 
