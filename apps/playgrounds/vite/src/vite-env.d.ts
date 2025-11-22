023cc7eb3d0f8448cab1562fa08c95a83d95f78e
Yam C Borodetsky <yam@yam.codes>
2025-11-23 02:02:07 +0500

    feat: enhance TypeScript type safety in Vite environment
    - Added TypeScript definitions for enhanced type safety in the Vite environment.
    - Introduced `ImportMetaEnvAugmented` and `ViteTypeOptions` interfaces to improve type checking for environment variables.
    - Updated the linter configuration to disable the `noUnusedVariables` rule for TypeScript declaration files.

--- apps/playgrounds/vite/src/vite-env.d.ts
+++ apps/playgrounds/vite/src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+type ImportMetaEnvAugmented =
+	import("@julr/vite-plugin-validate-env").ImportMetaEnvAugmented<
+		typeof import("../env").default
+	>;
+
+interface ViteTypeOptions {
+	// Avoid adding an index type to `ImportMetaDev` so
+	// there's an error when accessing unknown properties.
+	// ⚠️ This option requires Vite 6.3.x or higher
+	strictImportMetaEnv: unknown;
+}
+
+// Now import.meta.env is totally typesafe and based on your `Env` schema definition
+interface ImportMetaEnv extends ImportMetaEnvAugmented {}
