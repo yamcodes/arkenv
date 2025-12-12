77a77a192fe141a34ef43d16fcd33f40d9296ced
Yam C Borodetsky <yam@yam.codes>
2025-12-12 14:06:08 +0500

    refactor: replace two `createEnv` overloads for `SchemaShape` with one for `Record<string, unknown>`.

---
+++ .changeset/brown-dolls-poke.md
@@ -0,0 +1,7 @@
+---
+"@repo/scope": patch
+---
+
+#### Export $ type
+
+Export `type $ = (typeof $)["t"]` for convenience. This type is reused a lot in the other packages.
--- packages/internal/types/src/schema.ts
+++ packages/internal/types/src/schema.ts
@@ -6,4 +6,4 @@ export const SchemaShape = type({ "[string]": "unknown" });
 // Same thing as `typeof SchemaShape.infer` but more performant
 export type SchemaShape = Record<string, unknown>;
 
-export type EnvSchemaWithType = Type<SchemaShape, (typeof $)["t"]>;
+export type EnvSchemaWithType = Type<SchemaShape, $>;
