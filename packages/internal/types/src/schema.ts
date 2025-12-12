d12ee9995a921302de6d4d137ec8e3deb2c162d1
Yam C Borodetsky <yam@yam.codes>
2025-12-12 13:44:54 +0500

    feat: add `SchemaShape` constant and type definition for generic object schema

--- packages/internal/types/src/schema.ts
+++ packages/internal/types/src/schema.ts
@@ -1,5 +1,9 @@
 import type { $ } from "@repo/scope";
-import type { Type } from "arktype";
+import { type Type, type } from "arktype";
 
+export const SchemaShape = type({ "[string]": "unknown" });
+
+// Same thing as `typeof SchemaShape.infer` but more performant
 export type SchemaShape = Record<string, unknown>;
+
 export type EnvSchemaWithType = Type<SchemaShape, (typeof $)["t"]>;
