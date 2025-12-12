f374f42e9164e84f9c689989e1bc097f548f9ddb
Yam C Borodetsky <yam@yam.codes>
2025-12-12 18:21:18 +0500

    refactor: move `ImportMetaEnvAugmented` type definition to a dedicated file in vite-plugin.

--- packages/internal/types/src/index.ts
+++ packages/internal/types/src/index.ts
@@ -5,8 +5,8 @@
  * within the monorepo.
  */
 
-export type * from "./filter-by-prefix";
 // Only TypeScript types
+export type * from "./filter-by-prefix";
 export type * from "./infer-type";
 
 // Also includes ArkType types
