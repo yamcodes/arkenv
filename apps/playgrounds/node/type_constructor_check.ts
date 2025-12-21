02591dddd5cd306279b32bd716191775bb650848
Yam Borodetsky <yam@yam.codes>
2025-12-21 18:56:24 +0500

    chore: remove unused type constructor check file to clean up the codebase

--- apps/playgrounds/node/type_constructor_check.ts
+++
@@ -1,7 +0,0 @@
-import { type, Type } from "arktype";
-
-const n = type.number.internal;
-const t = new Type(n, type.$);
-console.log("t.expression:", t.expression);
-console.log("t.allows(5):", t.allows(5));
-console.log("t.allows('5'):", t.allows("5"));
