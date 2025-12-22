2d579f608406a45a0fff7fc75dcd528c68d28ef3
Yam C Borodetsky <yam@yam.codes>
2025-12-22 23:17:10 +0500

    refactor(www): Adjust Sentry webpack config
    - Move treeshake and annotation settings
    - Enable removeDebugLogging via webpack treeshake
    - Update automaticVercelMonitors placement

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -66,31 +66,32 @@ const sentryConfig = {
 	// Upload a larger set of source maps for prettier stack traces (increases build time)
 	widenClientFileUpload: true,
 
-	// Automatically annotate React components to show their full name in breadcrumbs and session replay
-	reactComponentAnnotation: {
-		enabled: true,
-	},
-
 	// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 	// This can increase your server load as well as your hosting bill.
 	// Note: Check that the configured route will not match with your Next.js proxy, otherwise reporting of client-
 	// side errors will fail.
 	tunnelRoute: "/monitoring",
 
-	// Automatically tree-shake Sentry logger statements to reduce bundle size
-	disableLogger: true,
-
-	// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-	// See the following for more information:
-	// https://docs.sentry.io/product/crons/
-	// https://vercel.com/docs/cron-jobs
-	automaticVercelMonitors: true,
-
 	sourcemaps: {
 		deleteSourcemapsAfterUpload: true,
 	},
 
 	authToken: process.env.SENTRY_AUTH_TOKEN,
+	webpack: {
+		treeshake: {
+			// Automatically tree-shake Sentry logger statements to reduce bundle size
+			removeDebugLogging: true,
+		},
+		// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+		// See the following for more information:
+		// https://docs.sentry.io/product/crons/
+		// https://vercel.com/docs/cron-jobs
+		automaticVercelMonitors: true,
+		// Automatically annotate React components to show their full name in breadcrumbs and session replay
+		reactComponentAnnotation: {
+			enabled: true,
+		},
+	},
 } as const satisfies SentryBuildOptions;
 
 export default withNextVideo(
