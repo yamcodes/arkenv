d242736ddedb4707b6e8f64495a602ff3bb7542f
Yam C Borodetsky <yam@yam.codes>
2026-01-04 23:57:32 +0500

    chore: Remove `MY_ARRAY` and `MY_MIXED_VALUE` environment variable definitions and their parsing.

--- apps/playgrounds/node/index.ts
+++ apps/playgrounds/node/index.ts
@@ -5,8 +5,6 @@ const env = arkenv({
 	PORT: "0 <= number.integer <= 65535",
 	NODE_ENV: "'development' | 'production' | 'test' = 'development'",
 	DEBUGGING: "boolean = false",
-	MY_ARRAY: "string[]",
-	MY_MIXED_VALUE: "string | number",
 });
 
 // Automatically validate and parse process.env
@@ -16,8 +14,6 @@ console.log({
 	port: env.PORT,
 	nodeEnv: env.NODE_ENV,
 	debugging: env.DEBUGGING,
-	myArray: env.MY_ARRAY,
-	myMixedValue: env.MY_MIXED_VALUE,
 });
 
 export default env;
