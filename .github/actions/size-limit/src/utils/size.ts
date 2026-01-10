f0a2961d0e46ed92753dbec076137c6313cb641a
Yam C Borodetsky <yam@yam.codes>
2026-01-10 23:58:00 +0500

    fix(size-limit): prevent size unit index out of bounds

--- .github/actions/size-limit/src/utils/size.ts
+++ .github/actions/size-limit/src/utils/size.ts
@@ -66,6 +66,9 @@ export const formatBytes = (bytes: number): string => {
 	if (bytes === 0) return "0 B";
 	const k = 1024;
 	const sizes = ["B", "kB", "MB", "GB", "TB"];
-	const i = Math.floor(Math.log(bytes) / Math.log(k));
+	const i = Math.min(
+		Math.floor(Math.log(bytes) / Math.log(k)),
+		sizes.length - 1,
+	);
 	return `${Number.parseFloat((bytes / k ** i).toFixed(2))} ${sizes[i]}`;
 };
