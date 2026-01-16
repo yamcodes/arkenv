288df4926993db7cdaeaa44acd2b2e9b2822ded7
Yam C Borodetsky <yam@yam.codes>
2026-01-16 21:59:07 +0500

    refactor: remove arktype dependency and switch to standard validator in without-arktype example.

--- examples/without-arktype/src/index.ts
+++ examples/without-arktype/src/index.ts
@@ -3,12 +3,11 @@ import z from "zod";
 
 const env = createEnv(
 	{
-		// TEST_VAL: "string",
 		TEST_VALUE: z.url(),
 		PORT: z.coerce.number(),
 		HOST: z.literal("localhost").or(z.url()),
 	},
-	{ validator: "arktype" },
+	{ validator: "standard" },
 );
 
 console.log(`Value: ${String(env.TEST_VALUE)}`);
