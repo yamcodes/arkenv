31cf341649d52fe0bd7ba0a6de35d6f769e3e39b
Yam C Borodetsky <yam@yam.codes>
2026-01-21 23:55:49 +0500

    Fix exports

--- packages/arkenv/src/index.ts
+++ packages/arkenv/src/index.ts
@@ -1,11 +1,13 @@
 import { createEnv } from "./create-env.ts";
-import { ArkEnvError } from "./errors.ts";
 
 export { createEnv };
 export type { ArkEnvConfig, EnvSchema } from "./create-env.ts";
 export { ArkEnvError } from "./errors.ts";
 
+/**
+ * ArkEnv's main export, an alias for {@link createEnv}
+ *
+ * {@link https://arkenv.js.org | ArkEnv} is a typesafe environment variables validator from editor to runtime.
+ */
 const arkenv = createEnv;
-
-export { arkenv, createEnv, ArkEnvError };
 export default arkenv;
